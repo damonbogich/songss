33a28922c1e9a4ffcc81517f5c97d952e42a9b14
damonbogich <damonbogich@gmail.com>
2020-02-13 13:30:04 -0500

    make port dynamic

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ server.get("/", (req, res) => {
     res.status(200).json(songs);
 });
 
-
-const port = 5000;
+//make port dynamic
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on Port ${port}`))
